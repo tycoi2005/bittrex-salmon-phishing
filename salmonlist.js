4ea84e01ee6c1213944f9befe6887df23427b66d
toanhd <toanhd@cardano-labo>
2017-07-06 00:21:09 +0700

    format view

--- salmonlist.js
+++ salmonlist.js
@@ -1,13 +1,20 @@
 var bittrex = require('node.bittrex.api');
+var numeral = require('numeral');
+
 const prefix = 'https://bittrex.com/Market/Index?MarketName=';
 function calcTangNumber(obj){
 	obj.TangNumber = ((obj.High-obj.Last) - (obj.Last - obj.Low))/(obj.Last)
 
+    //obj.TangNumber = ((obj.High-obj.Last))/(obj.Last)
     // obj.TangNumber = ((obj.High-obj.Last) - (obj.Last - obj.Low))/(obj.High-obj.Low) // coin giam dan deu
     // obj.TangNumber = -((obj.High-obj.Last) - (obj.Last - obj.Low))/(obj.High-obj.Low) // coin tang dan deu
 	if (! /^BTC.*/.test(obj.MarketName)){
 		obj.TangNumber = -100;
 	}
+}
+
+function format(number){
+    return numeral(number).format('0.000');
 }
 
 function compareObject(a, b){
@@ -27,8 +34,9 @@ bittrex.getmarketsummaries( function( data ) {
   var list = data.result;
   list.sort(compareObject);
 
-  for (var i =0; i< 20; i++){
+    console.log("ID\t", prefix + "Market"  , "\t" , "TangNumber", "\tk", "Volume", "\t", "Last");
+  for (var i =0; i< 30; i++){
   	var item = list[i]
-  	console.log(prefix + item.MarketName  , " : " , item.TangNumber, " : ", item.Volume, " , ", item.Last);
+  	console.log("", i, '\t', prefix + item.MarketName  , "\t" , format(item.TangNumber), "\t", format(item.Volume), "\t", item.Last);
   }
 });
